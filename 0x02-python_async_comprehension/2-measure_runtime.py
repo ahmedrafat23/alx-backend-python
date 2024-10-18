7fd77bdcbb906585243aae46d8e67c71b5eb2fed
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2024-10-18 23:32:41 +0000

    a

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
+"""
+Measures the total execution time for wait_n(n, max_delay)
+"""
 import asyncio
 import time
-from 1_async_comprehension import async_comprehension
 
-async def measure_runtime() -> float:
-    """Measure the total runtime of running async_comprehension four times in parallel."""
-    start_time = time.perf_counter()
-    
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    
-    end_time = time.perf_counter()
-    return end_time - start_time
 
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure_time function with integers n and max_delay as arguments
+
+    Args:
+        n(int): times of spawn
+        max_delay(int): maximum amount of delay in second
+
+    Returns:
+        returns total_time / n
+    """
+    wait_n = __import__('1-concurrent_coroutines').wait_n
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+
+    total_time = time.time() - start
+    return total_time / n
