93cc0009cf941b9a652c57de6947a1a89f18f072
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2024-10-18 23:36:06 +0000

    a

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,29 +1,8 @@
 #!/usr/bin/env python3
-"""
-You will spawn wait_random n times with the specified max_delay
-"""
-import asyncio
 from typing import List
+from 0_async_generator import async_generator
 
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers using an async comprehension and return them."""
+    return [i async for i in async_generator()]
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Wait_n should return the list of all the delays (float values)
-
-    Args:
-        n(int): times of spawn
-        max_delay(int): maximum amount of delay in second
-
-    Returns:
-        delay(list[float]): List of the delays in ascending order
-    """
-    delays = []
-
-    wait_random = __import__('0-basic_async_syntax').wait_random
-    tasks = [wait_random(max_delay) for _ in range(n)]
-
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-
-    return delays
